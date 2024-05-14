e1c6ddb3eff66b01550961f1ea03dd92c4c6acb7
Yiting <ftyuk21@gmail.com>
2024-05-14 23:24:11 +0800

    debug

--- maas/src/app/pages/waiting/waiting.module.ts
+++ maas/src/app/pages/waiting/waiting.module.ts
@@ -5,7 +5,7 @@ import { WaitingComponent } from './waiting.component';
 import { RouterModule, Routes } from '@angular/router';
 import { WaitingConfirmComponent } from './waiting-confirm/waiting-confirm.component';
 export const routes: Routes = [
-  { path: '', component: WaitingComponent, pathMatch: 'full' }
+  { path: '', component: WaitingComponent, pathMatch: 'full' },
   { path: '', component: WaitingConfirmComponent, pathMatch: 'full' }
 ];
 
