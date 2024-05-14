3459e36ced8fdbdba7c96fdaa5824c216bffa756
MattChuang0 <bxiang0520@icloud.com>
2024-05-14 20:47:25 +0800

    ?!

---
+++ maas/src/app/pages/waiting/waiting-confirm/waiting-confirm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-confirm',
+  templateUrl: './waiting-confirm.component.html',
+  styleUrls: ['./waiting-confirm.component.scss']
+})
+export class WaitingConfirmComponent {
+
+}
--- maas/src/app/pages/waiting/waiting.module.ts
+++ maas/src/app/pages/waiting/waiting.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { WaitingComponent } from './waiting.component';
 import { RouterModule, Routes } from '@angular/router';
+import { WaitingConfirmComponent } from './waiting-confirm/waiting-confirm.component';
 export const routes: Routes = [
   { path: '', component: WaitingComponent, pathMatch: 'full' }
+  { path: '', component: WaitingConfirmComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
-    WaitingComponent
+    WaitingComponent,
+    WaitingConfirmComponent
   ],
   imports: [
     CommonModule,
