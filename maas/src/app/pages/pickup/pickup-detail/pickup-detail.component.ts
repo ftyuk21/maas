fcd0dcb182b1e330561f2c09a37e3f257b075cda
Yiting <ftyuk21@gmail.com>
2024-05-14 20:14:27 +0800

    接單詳細頁面

--- maas/src/app/pages/pickup/pickup-detail/pickup-detail.component.ts
+++ maas/src/app/pages/pickup/pickup-detail/pickup-detail.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -10,6 +11,8 @@ export class PickupDetailComponent implements OnInit {
   id: any;
   comment:{}[];
 
+  progressBarCount;
+
   order = {
     orderId: 5,
     orderCode: "3578465",
@@ -20,14 +23,14 @@ export class PickupDetailComponent implements OnInit {
       { passengerId: 2, passengerName: "BBB", rating: 5, comment: "給小費" },
       { passengerId: 1, passengerName: "AAA", rating: 5, comment: "有小費" },
       { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
+      { passengerId: 4, passengerName: "DDD", rating: 2.5, comment: "態度很差" },
       { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
       { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
       { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
-      { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
-      { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
-      { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
-      { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
-      { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
+      { passengerId: 4, passengerName: "DDD", rating: 3.4, comment: "態度很差" },
+      { passengerId: 4, passengerName: "DDD", rating: 5, comment: "態度很差" },
+      { passengerId: 4, passengerName: "DDD", rating: 1.7, comment: "態度很差" },
+      { passengerId: 4, passengerName: "DDD", rating: 5, comment: "態度很差" },
     ],
     time: "2024-05-14T12:30:00Z",
     startPlace: "XXXXXXXXXXXXXXXXXXXXX",
@@ -44,14 +47,27 @@ export class PickupDetailComponent implements OnInit {
   markerPositions: google.maps.LatLngLiteral[] = [
     { lat: 40.678178, lng: -73.944158 }
   ];
-  constructor(private route: ActivatedRoute,) { }
+  constructor(private route: ActivatedRoute, public http: HttpClient) { }
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = params['id']; 
       this.comment = this.order.comment;
       this.ratingsCount = this.order.comment.length;
       this.ratingsValue = this.order.comment.reduce((total, comment) => total + comment.rating, 0);
+      this.progressBarCount = Array.from({ length: 5 }, (_, i) => {
+        console.log(i)
+        const rating = 5 - i; // 從 5 開始遞減
+        const count = this.order.comment.filter(comment => Math.floor(comment.rating) === rating).length;
+        return count;
+      });
+      console.log(this.progressBarCount)
     });
+  }
+
+  accept(){
+    this.http.get<any>("test/hello").subscribe(data => {
+      console.log(data)
+    })
   }
 
 }
