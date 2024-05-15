1873ef54d440b45731b2da178300f629eac526ac
PhoenixHung <phoenix000066@gmail.com>
2024-05-15 15:45:21 +0800

    API 1.1

--- MaasService/src/main/java/ndhu/tw/MaasService/model/request/BookingRequestModel.java
+++ MaasService/src/main/java/ndhu/tw/MaasService/model/request/BookingRequestModel.java
@@ -1,51 +1,57 @@
 package ndhu.tw.MaasService.model.request;
 
+import lombok.Data;
+
+@Data
 public class BookingRequestModel {
     private String phoneNumber;
     private String startLocation;
     private String destination;
     private String pickupTime;
-    private String priceRange;
+    private Integer priceRangeUp;
+    private Integer priceRangeDown;
+    private Integer userID;
 
-    // Getters and Setters
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getStartLocation() {
-        return startLocation;
-    }
-
-    public void setStartLocation(String startLocation) {
-        this.startLocation = startLocation;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getPickupTime() {
-        return pickupTime;
-    }
-
-    public void setPickupTime(String pickupTime) {
-        this.pickupTime = pickupTime;
-    }
-
-    public String getPriceRange() {
-        return priceRange;
-    }
-
-    public void setPriceRange(String priceRange) {
-        this.priceRange = priceRange;
-    }
+//    Getters and Setters
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getStartLocation() {
+//        return startLocation;
+//    }
+//
+//    public void setStartLocation(String startLocation) {
+//        this.startLocation = startLocation;
+//    }
+//
+//    public String getDestination() {
+//        return destination;
+//    }
+//
+//    public void setDestination(String destination) {
+//        this.destination = destination;
+//    }
+//
+//    public String getPickupTime() {
+//        return pickupTime;
+//    }
+//
+//    public void setPickupTime(String pickupTime) {
+//        this.pickupTime = pickupTime;
+//    }
+//
+//    public String getPriceRange() {
+//        return priceRange;
+//    }
+//
+//    public void setPriceRange(String priceRange) {
+//        this.priceRange = priceRange;
+//    }
 }
 
--- MaasService/src/main/java/ndhu/tw/MaasService/service/MaasService.java
+++ MaasService/src/main/java/ndhu/tw/MaasService/service/MaasService.java
@@ -4,12 +4,19 @@ import ndhu.tw.MaasService.model.request.BookingRequestModel;
 import ndhu.tw.MaasService.model.response.BookingResponseModel;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class MaasService {
 
     public BookingResponseModel createBooking(BookingRequestModel request) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 7; i++) {
+            sb.append(random.nextInt(10));
+        }
         // 這裡會有實際的邏輯去處理訂單創建
-        String orderNumber = "ORD123456"; // 假設生成一個訂單編號
+        String orderNumber = "ORD"+sb; // 假設生成一個訂單編號
         String status = "等待接單"; // 設定狀態
 
         return new BookingResponseModel(orderNumber, status);
