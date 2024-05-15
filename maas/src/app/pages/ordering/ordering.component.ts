0d5f704836361cab13d189952596cc462a18f4d1
Yiting <ftyuk21@gmail.com>
2024-05-15 18:21:28 +0800

    下單頁面串後端並更新google地圖

--- maas/src/app/pages/ordering/ordering.component.ts
+++ maas/src/app/pages/ordering/ordering.component.ts
@@ -1,10 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import {
   UntypedFormBuilder,
   UntypedFormGroup,
   Validators,
 } from "@angular/forms";
+import { GoogleMap } from "@angular/google-maps";
+import { ActivatedRoute, Router } from "@angular/router";
+import { GoogleService } from "src/app/shared/service/google.service";
+import { NotificationService } from "src/app/shared/service/notification.service";
 
+function convertToLatLngLiteral(position: { lat: number, lng: number }): google.maps.LatLngLiteral {
+  return { lat: position.lat, lng: position.lng };
+}
+
+export class Options {
+  position: google.maps.LatLngLiteral;
+  label: { color: string, text: string, fontSize: string, fontWeight: string };
+  title: string;
+  options: google.maps.MarkerOptions;
+  // icon: {
+  //   url: string, // icon URL
+  //   scaledSize: google.maps.Size; // 尺寸(32, 32)
+  //   origin: google.maps.Point; // 原點(0, 0)
+  //   anchor: google.maps.Point; // 錨點(16, 16)
+  // };
+
+}
 
 @Component({
   selector: "app-ordering",
@@ -15,14 +37,18 @@ export class OrderingComponent implements OnInit {
   orderForm: UntypedFormGroup; // form
   userInfo: any = {}; // 定義 物件
 
-  center: google.maps.LatLngLiteral = { lat: 40.678178, lng: -73.944158 };
-  zoom = 7;
-  markerOptions: google.maps.MarkerOptions = { draggable: false };
-  markerPositions: google.maps.LatLngLiteral[] = [
-    { lat: 40.678178, lng: -73.944158 }
-  ];
+  center: google.maps.LatLngLiteral; // 地圖中心點
+  zoom = 15; // 縮放
+  markers: {
+    options: Options[];
+    position: google.maps.LatLngLiteral[];
+  } = { options: [], position: [] };
 
-  constructor(public fb: UntypedFormBuilder) {}
+  @ViewChild('googleMap') googleMap: GoogleMap;
+
+  constructor(public fb: UntypedFormBuilder, private route: ActivatedRoute, private router: Router,
+    public http: HttpClient, public map: GoogleService,
+    public msg: NotificationService) { }
 
   ngOnInit() {
     // 取得資料
@@ -31,23 +57,53 @@ export class OrderingComponent implements OnInit {
     this.getUserInfo();
     this.initForm();
     this.orderForm.patchValue({
-      phone: this.userInfo.phone,
+      userID: this.userInfo.id,
+      phoneNumber: this.userInfo.phone,
     });
+
+    this.map.getLocation()
+      .then(coords => {
+        this.center = { lat: coords.latitude, lng: coords.longitude }
+        const o = {
+          position: { lat: coords.latitude, lng: coords.longitude },
+          label: {
+            color: "black", text: "當前", fontSize: '15px', fontWeight: 'bold',
+          },
+          title: "當前位置",
+          options: { draggable: false },
+          // icon: {
+          //   url: 'http://maps.google.com/mapfiles/ms/micons/red.png',
+          //   scaledSize: new google.maps.Size(45, 45), // 尺寸(32, 32)
+          //   origin: new google.maps.Point(0, 0), // 原點(0, 0)
+          //   anchor: new google.maps.Point(16, 16) // 錨點(16, 16)
+          // }
+        }
+        this.setGooglePostion(o)
+      })
+      .catch(error => {
+        this.msg.showError("取得當前位置錯誤，請聯繫技術人員！", "錯誤")
+        console.error(error);
+      });
   }
 
   initForm() {
     this.orderForm = this.fb.group({
-      phone: ["", Validators.compose([Validators.required])],
-      addressfrom: [
-        "",
-        Validators.compose([Validators.required, Validators.minLength(5)]),
-      ],
-      addressto: [
-        "",
-        Validators.compose([Validators.required, Validators.minLength(5)]),
-      ],
-      priceupto: ["", Validators.compose([Validators.required])],
-      pricedownto: ["", Validators.compose([Validators.required])],
+      userID: [0, Validators.compose([Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required])],
+      startLocation: [null, Validators.compose([Validators.required])],
+      destination: [null, Validators.compose([Validators.required])],
+      pickupTime: [null, Validators.compose([Validators.required])],
+      priceRangeUp: [null, Validators.compose([Validators.required])],
+      priceRangeDown: [null, Validators.compose([Validators.required])],
+    });
+
+
+    this.orderForm.get('startLocation').valueChanges.subscribe(value => {
+      this.showInMap(value, '起始地');
+    });
+
+    this.orderForm.get('destination').valueChanges.subscribe(value => {
+      this.showInMap(value, '目的地');
     });
   }
 
@@ -57,14 +113,92 @@ export class OrderingComponent implements OnInit {
   }
 
   sendOrder() {
-    // 檢查該填寫的有沒有填寫
-    // 如果沒有  提醒他填好
-    // 如果有  發送給後端 讓訂單成立
-    console.log(this.orderForm.value);
+    this.markAllAsTouchedAndDirty();
     if (this.orderForm.valid) {
-      console.log("符合規定");
+      this.http.post<any>("passenger/bookings", this.orderForm.getRawValue()).subscribe(data => {
+        this.msg.showSuccess("下單成功，將為您配對合適的司機！", "")
+        console.log(data)
+        this.router.navigate(['/waiting']); // 导航到其他页面
+      })
     } else {
-      console.log("不符合規定");
+      this.msg.showError("資料填寫不完整", "")
     }
+  }
+
+  setGooglePostion(postion: Options) {
+    this.markers.options.push(postion as Options)
+    this.markers.position.push(postion.position)
+    console.log(this.markers)
+
+    // this.markers = {
+    //   options: [
+    //     {
+    //       position: convertToLatLngLiteral({ lat: this.latitude, lng: this.longitude }),
+    //       label: { color: 'red', text: '您的當前位置 ' },
+    //       title: '當前位置',
+    //       options: { draggable: false },
+    //     },
+    //     {
+    //       position: convertToLatLngLiteral({ lat: this.latitude, lng: this.longitude }),
+    //       label: { color: 'red', text: '您的當前位置 ' },
+    //       title: '當前位置',
+    //       options: { draggable: false },
+    //     },
+    //   ],
+    //   position: [
+    //     { lat: this.latitude, lng: this.longitude }, // 當前位置
+    //     { lat: this.latitude + 0.05, lng: this.longitude }, // 起始位置
+    //     { lat: this.latitude + 0.06, lng: this.longitude }, // 結束位置
+    //   ]
+    // }
+  }
+
+  showInMap(value: string, type: string) {
+    this.map.getCoordinates(value).subscribe(data => {
+      console.log(data)
+      if (data.status == "OK") {
+        if (type == '起始') {
+          const o = {
+            position: data.results[0].geometry.location,
+            label: {
+              color: "black", text: "當前", fontSize: '15px', fontWeight: 'bold',
+            },
+            title: "當前位置",
+            options: { draggable: false },
+          }
+          console.log(this.markers)
+          this.setGooglePostion(o)
+
+
+          console.log(data)
+        } else {
+
+        }
+      } else {
+        this.msg.showError('請輸入有效地址');
+      }
+
+    })
+  }
+
+  // 重新加载地图
+  reloadMap() {
+    if (this.googleMap) {
+      const bounds = new google.maps.LatLngBounds();
+      for (const position of this.markers.position) {
+        bounds.extend(position);
+      }
+
+      // 调整地图以适应边界
+      this.googleMap.fitBounds(bounds);
+    }
+  }
+
+  markAllAsTouchedAndDirty(): void {
+    Object.keys(this.orderForm.controls).forEach(key => {
+      const control = this.orderForm.get(key);
+      control.markAsTouched();
+      control.markAsDirty();
+    });
   }
 }
--- maas/src/app/pages/pickup/pickup-detail/pickup-detail.component.ts
+++ maas/src/app/pages/pickup/pickup-detail/pickup-detail.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WaitDialogComponent } from '../wait-dialog/wait-dialog.component';
+import { GoogleService } from 'src/app/shared/service/google.service';
+import { NotificationService } from 'src/app/shared/service/notification.service';
 
+function convertToLatLngLiteral(position: { lat: number, lng: number }): google.maps.LatLngLiteral {
+  return { lat: position.lat, lng: position.lng };
+}
 @Component({
   selector: 'app-pickup-detail',
   templateUrl: './pickup-detail.component.html',
@@ -11,17 +16,17 @@ import { WaitDialogComponent } from '../wait-dialog/wait-dialog.component';
 })
 export class PickupDetailComponent implements OnInit {
   id: any;
-  comment:{}[];
+  comment: {}[];
 
   progressBarCount;
 
   order = {
     orderId: 5,
     orderCode: "3578465",
-    passengerRatingCount: 13, 
+    passengerRatingCount: 13,
     passengerRatingValue: 55,
     comment: [
-      { passengerId: 3, passengerName: "CCC", rating: 4.5 , comment :"乘客有禮貌"},
+      { passengerId: 3, passengerName: "CCC", rating: 4.5, comment: "乘客有禮貌" },
       { passengerId: 2, passengerName: "BBB", rating: 5, comment: "給小費" },
       { passengerId: 1, passengerName: "AAA", rating: 5, comment: "有小費" },
       { passengerId: 4, passengerName: "DDD", rating: 2, comment: "態度很差" },
@@ -35,38 +40,123 @@ export class PickupDetailComponent implements OnInit {
       { passengerId: 4, passengerName: "DDD", rating: 5, comment: "態度很差" },
     ],
     time: "2024-05-14T12:30:00Z",
-    startPlace: "XXXXXXXXXXXXXXXXXXXXX",
-    endPlace: "XXXXXXXXXXXXXXXXXXXXX"
+    startPlace: "974花蓮縣壽豐鄉理工二館",
+    endPlace: "974花蓮縣壽豐鄉中正路1號"
   }
+
+  latitude: number; // 緯度 當前位置
+  longitude: number; // 經度 當前位置
 
 
   ratingsCount: any; // 評分人數
   ratingsValue: any; // 總分
 
-  center: google.maps.LatLngLiteral = { lat: 40.678178, lng: -73.944158 };
-  zoom = 7;
-  markerOptions: google.maps.MarkerOptions = { draggable: false };
-  markerPositions: google.maps.LatLngLiteral[] = [
-    { lat: 40.678178, lng: -73.944158 }
-  ];
-  constructor(private route: ActivatedRoute, public http: HttpClient, public dialog: MatDialog) { }
+  center: google.maps.LatLngLiteral; // 地圖中心點
+  zoom = 15; // 縮放
+  markerOptions: google.maps.MarkerOptions; // 標記點(司機位置)
+  startMarkerOptions: google.maps.MarkerOptions; // 標記點(起始位置)
+  endMarkerOptions: google.maps.MarkerOptions; // 標記點(抵達位置)
+  markerPositions: google.maps.LatLngLiteral[]; // 儲存點的陣列
+
+  markers: {
+    options: {
+      position: google.maps.LatLngLiteral,
+      label: { color: string, text: string },
+      title: string,
+      options: google.maps.MarkerOptions,
+    }[];
+    position: google.maps.LatLngLiteral[];
+  }
+
+  test = "測試"
+
+  constructor(private route: ActivatedRoute,
+    public http: HttpClient, public dialog: MatDialog, public map: GoogleService,
+    public msg: NotificationService) { }
+
+
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.id = params['id']; 
+      this.id = params['id'];
       this.comment = this.order.comment;
       this.ratingsCount = this.order.comment.length;
       this.ratingsValue = this.order.comment.reduce((total, comment) => total + comment.rating, 0);
       this.progressBarCount = Array.from({ length: 5 }, (_, i) => {
-        console.log(i)
         const rating = 5 - i; // 從 5 開始遞減
         const count = this.order.comment.filter(comment => Math.floor(comment.rating) === rating).length;
         return count;
       });
-      console.log(this.progressBarCount)
+
+      this.map.getCoordinates(this.order.startPlace).subscribe(data => {
+        const driverPosition: google.maps.LatLngLiteral = { lat: this.latitude, lng: this.longitude };
+        this.startMarkerOptions = {
+          position: driverPosition,
+          label: '您的所在地' // 標籤
+        };
+      })
+
+      this.map.getCoordinates(this.order.endPlace).subscribe(data => {
+        if (data.status == 'OK') {
+          // const endPosition: google.maps.LatLngLiteral = data.results.location;
+          this.endMarkerOptions = {
+            position: data.results.location,
+            label: '乘客目的地' // 標籤
+          };
+        }
+        console.log(data)
+
+      })
+
     });
+
+    this.map.getLocation()
+      .then(coords => {
+        this.latitude = coords.latitude;
+        this.longitude = coords.longitude;
+        this.setGooglePostion();
+      })
+      .catch(error => {
+        this.msg.showError("取得當前位置錯誤，請聯繫技術人員！", "錯誤")
+        console.error(error);
+      });
+
   }
 
-  accept(){
+  setGooglePostion() {
+    this.center = { lat: this.latitude, lng: this.longitude };
+    this.markerOptions = { draggable: false };
+    this.startMarkerOptions = { draggable: false };
+    this.endMarkerOptions = { draggable: false };
+    this.markerPositions = [
+      { lat: this.latitude, lng: this.longitude }, // 當前位置
+      { lat: this.latitude + 0.05, lng: this.longitude }, // 起始位置
+      { lat: this.latitude + 0.06, lng: this.longitude }, // 結束位置
+    ];
+
+    this.markers = {
+      options: [
+        {
+          position: convertToLatLngLiteral({ lat: this.latitude, lng: this.longitude }),
+          label: { color: 'red', text: '您的當前位置 ' },
+          title: '當前位置',
+          options: { draggable: false },
+        },
+        {
+          position: convertToLatLngLiteral({ lat: this.latitude, lng: this.longitude }),
+          label: { color: 'red', text: '您的當前位置 ' },
+          title: '當前位置',
+          options: { draggable: false },
+        },
+      ],
+      position: [
+        { lat: this.latitude, lng: this.longitude }, // 當前位置
+        { lat: this.latitude + 0.05, lng: this.longitude }, // 起始位置
+        { lat: this.latitude + 0.06, lng: this.longitude }, // 結束位置
+      ]
+    }
+  }
+
+  accept() {
     this.http.get<any>("test/hello").subscribe(data => {
       console.log(data)
       const dialogRef = this.dialog.open(WaitDialogComponent, {
