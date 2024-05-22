8f420fa4baeb632d9e1a0cc5edfeb86485994f22
Yiting <ftyuk21@gmail.com>
2024-05-22 10:51:03 +0800

    swagger 加入流程

--- MaasService/src/main/java/ndhu/tw/MaasService/SpringDocConfig/SpringDocConfig.java
+++ MaasService/src/main/java/ndhu/tw/MaasService/SpringDocConfig/SpringDocConfig.java
@@ -12,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
 	info = @Info(
 		title = "MaasService",
-		version = "1.0.0"
+		version = "1.0.0",
+		description = "流程: 1.1 -> 2.1 -> 2.2 -> 1.2"
 	),
 	servers = {
   	@Server(url = "${server.swagger.url}", description = "Default Server URL")
