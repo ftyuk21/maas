56b6f2bfd453e13f6bb28d37c8e9a36bcc468879
YiTing <ftyuk21@gmail.com>
2024-06-08 00:00:18 +0800

    修正設定

---
+++ MaasService/src/main/java/ndhu/tw/MaasService/auth/CorsFilterConfig.java
@@ -0,0 +1,36 @@
+package ndhu.tw.MaasService.auth;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilterConfig extends OncePerRequestFilter {
+
+    /**
+     * CORS filter for http-request and response
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+}
--- MaasService/src/main/java/ndhu/tw/MaasService/auth/SecurityConfiguration.java
+++ MaasService/src/main/java/ndhu/tw/MaasService/auth/SecurityConfiguration.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -35,7 +38,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
@@ -45,23 +48,58 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
+                .addFilterBefore(new CorsFilterConfig(), ChannelProcessingFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+//        http.addFilterBefore(new CorsFilterConfig(), ChannelProcessingFilter.class);
         return http.build();
     }
 
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+////        CorsConfiguration configuration = new CorsConfiguration();
+//
+////        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
+////        configuration.setAllowedOrigins(Arrays.asList(
+////                "http://localhost:8005",
+////                "http://134.208.97.247:7001/",
+////                "http://127.0.0.1:7001/",
+////                "http://134.208.97.247:7000/",
+////                "http://127.0.0.1:7000/"
+////        ));
+////        configuration.setAllowedMethods(List.of("GET","POST"));
+////        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+//
+////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////
+////        source.registerCorsConfiguration("/**",configuration);
+//
+//        // v2
+////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////        CorsConfiguration config = new CorsConfiguration();
+////        config.setAllowCredentials(true); // 允許帶有認證信息的請求
+////        config.addAllowedOrigin("http://134.208.97.247:7001"); // 允許的來源
+////        config.addAllowedHeader("*"); // 允許所有標頭
+////        config.addAllowedMethod("*"); // 允許所有方法
+////        source.registerCorsConfiguration("/**", config);
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(false); // 不允许带有认证信息的请求
+//        config.addAllowedOrigin("*"); // 允许所有来源
+//        config.addAllowedHeader("*"); // 允许所有标头
+//        config.addAllowedMethod("*"); // 允许所有方法
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
+
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
-        configuration.setAllowedMethods(List.of("GET","POST"));
-        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**",configuration);
-
-        return source;
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**","/authority/**","/swagger-ui/**/","/control/**","rest-api-docs");
+//        return (web) -> web.ignoring().requestMatchers("app/**")
+//                .requestMatchers("/swagger-ui/**")
+//                .requestMatchers("/rest-api-docs/**")
+//                .requestMatchers("swagger-ui/**/").requestMatchers("swagger-ui/**/**/");
     }
 }
